267e5ddb81c4c1b5edfebf21cf8e982056abda48
vivexplorer <vivaanshah06@gmail.com>
2023-10-14 21:11:18 -0400

    Version 1!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/IndividualEncoder.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import static org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer.WHEEL_RADIUS;
+import static org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer.GEAR_RATIO;
+import static org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer.TICKS_PER_REV;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.drive.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.teamcode.util.Encoder;
+
+@Config
+
+@Autonomous(name = "Individual Encoder Testing")
+
+public class IndividualEncoder extends LinearOpMode {
+
+    double pi = 3.14;
+
+
+    private Encoder leftEncoder, rightEncoder, frontEncoder;
+
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "front right"));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "back left"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "back right"));
+
+
+
+
+
+        waitForStart();
+
+
+
+        while (opModeIsActive()){
+            double leftPos = (WHEEL_RADIUS * 2 *pi * GEAR_RATIO * leftEncoder.getCurrentPosition() / TICKS_PER_REV);
+            double rightPos = (WHEEL_RADIUS * 2 *pi * GEAR_RATIO * rightEncoder.getCurrentPosition() / TICKS_PER_REV);
+            double frontPos = (WHEEL_RADIUS * 2 *pi * GEAR_RATIO * frontEncoder.getCurrentPosition() / TICKS_PER_REV);
+            telemetry.addData("Left Pos", leftPos);
+            telemetry.addData("Right Pos", rightPos);
+            telemetry.addData("Front Pos", frontPos);
+            telemetry.update();
+
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/PIDF_Arm_Tuning.java
@@ -0,0 +1,56 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous
+
+public class PIDF_Arm_Tuning extends LinearOpMode {
+    private PIDController controller;
+
+    public static double p=0, i = 0, d=0;
+    public static double f = 0;
+
+    public static int target = 0;
+
+    private final double ticks_in_degrees = 0;
+
+    private DcMotorEx arm_motor;
+
+    @Override
+    public void runOpMode() {
+
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        arm_motor = hardwareMap.get(DcMotorEx.class, "lift_motor");
+        waitForStart();
+
+        while (opModeIsActive()) {
+            controller.setPID(p, i, d);
+            int armPos = arm_motor.getCurrentPosition();
+            double pid = controller.calculate(armPos, target);
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degrees)) * f;
+
+            double power = pid + ff;
+
+            arm_motor.setPower(power);
+
+            telemetry.addData("pos", armPos);
+            telemetry.addData("target", target);
+            telemetry.update();
+        }
+    }
+
+
+
+
+
+
+}
+
